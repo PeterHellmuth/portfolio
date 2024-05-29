90b8f4544509ce321b22bcf182687d19a09838a2
Peter <10523853+PeterHellmuth@users.noreply.github.com>
2024-05-29 13:26:34 -0500

    Add security group and existing SSH key

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,10 +10,49 @@ terraform {
 provider "aws" {
   region = "us-west-2"
 }
+
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+ ingress                = [
+   {
+     cidr_blocks      = [ "0.0.0.0/0", ]
+     description      = ""
+     from_port        = 22
+     ipv6_cidr_blocks = []
+     prefix_list_ids  = []
+     protocol         = "tcp"
+     security_groups  = []
+     self             = false
+     to_port          = 22
+  }
+  ]
+}
+
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+  count = 1
   tags = {
     Name = "portfolio"
   }
+  vpc_security_group_ids = [aws_security_group.main.id]
+  associate_public_ip_address = true
+  key_name="ssh-key"
+}
+
+output "instance_ip" {
+  description = "The public ip for ssh access"
+  value       = aws_instance.app_server[0].public_ip
 }
