91213d57aa92615936850dfa68d434ca4a1e4de4
Peter <10523853+PeterHellmuth@users.noreply.github.com>
2024-05-29 15:07:54 -0500

    Add 3000 tcp ingress, change pm2 start to use node www

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,7 +28,7 @@ resource "aws_security_group" "main" {
  ingress                = [
    {
      cidr_blocks      = [ "0.0.0.0/0", ]
-     description      = ""
+     description      = "ssh"
      from_port        = 22
      ipv6_cidr_blocks = []
      prefix_list_ids  = []
@@ -36,6 +36,17 @@ resource "aws_security_group" "main" {
      security_groups  = []
      self             = false
      to_port          = 22
+  },
+     {
+     cidr_blocks      = [ "0.0.0.0/0", ]
+     description      = "node-server"
+     from_port        = 3000
+     ipv6_cidr_blocks = []
+     prefix_list_ids  = []
+     protocol         = "tcp"
+     security_groups  = []
+     self             = false
+     to_port          = 3000
   }
   ]
 }
