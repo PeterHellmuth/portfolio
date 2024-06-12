6ed540b2505b60480561ef7a0d793402787090e8
Peter <10523853+PeterHellmuth@users.noreply.github.com>
2024-06-12 12:47:34 -0500

    Convert test to inventory-application with separate docker containers for front and backend. Remove unnecessary exposed ports in docker-compose.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <a href="https://www.google.com">google.com</a> test link
+        <a href="https://www.peterhellmuth.com/test-application">
+          test-application
+        </a>{" "}
+        test link
       </div>
     </main>
   );
